ecdaecc3bc3804428e3556ea830025e4ab1ca765
jiexdrop <jorge.noogueira@gmail.com>
2026-04-10 20:20:55 +0100

    added school stuff

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 @onready var mother: Sprite2D = $Mother
 @onready var father: Sprite2D = $Father
+@onready var load_scene_timer: Timer = $LoadSceneTimer
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	father.texture = preload("res://images/father/father.png")
 	mother.texture = preload("res://images/mother/mother.png")
+	load_scene_timer.start()
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
